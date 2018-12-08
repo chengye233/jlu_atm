9bc92046da52f55b70389dc697c34ae3e25c6a90
chengye233 <lijiacheng.ye@gmail.com>
2018-12-08 18:58:53 +0800

    Admin的insert。

---
+++ src/test/jlu/vending/dao/AdminDaoTest1.java
@@ -0,0 +1,46 @@
+package jlu.vending.dao;
+
+import jlu.vending.BaseTest;
+import jlu.vending.entity.Admin;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+import javax.annotation.Resource;
+
+import static org.junit.Assert.*;
+
+public class AdminDaoTest1 extends BaseTest {
+    @Autowired
+    private AdminDao adminDao;
+
+    @Autowired
+    PlatformTransactionManager transactionManager;
+
+
+
+    /**
+     * testInsertAdmin
+     */
+    @Test
+    public void testInsertAdmin() {
+        TransactionDefinition def = new DefaultTransactionDefinition();
+        TransactionStatus status = transactionManager.getTransaction(def);
+
+        try{
+          Admin admin = new Admin();
+          admin.setName("dog f");
+          admin.setPassword("wangwang");
+          adminDao.insertAdmin(admin);
+          assertEquals(6, (long) admin.getId());
+        } finally {
+            transactionManager.rollback(status);
+        }
+    }
+
+}
