d9a0747b53b4640ec3ed4ccf8b146ad6124690c5
chengye233 <lijiacheng.ye@gmail.com>
2018-12-10 18:12:15 +0800

    修改html引用css的路径为启动服务器后的路径。

--- src/main/java/jlu/vending/dao/AdminDao.java
+++ src/main/java/jlu/vending/dao/AdminDao.java
@@ -11,7 +11,7 @@ public interface AdminDao {
     /**
      * 插入{@link Admin}管理员
      * @param admin 要插入的管理员
-     * @return int 插入的管理员id
+     * @return Integer 插入的管理员id
      */
-    int insertAdmin(Admin admin);
+    Integer insertAdmin(Admin admin);
 }
