b8d0ef3c02dd73768b8f5a3640c248f156f78e88
IkigaiSeitetsu <keruberosu@qq.com>
2018-12-12 16:20:25 +0800

    insertcoin

---
+++ src/main/webapp/WEB-INF/html/script/itemdetail_purchase.js
@@ -0,0 +1,24 @@
+﻿var currentCoinValue = 0;
+var itemPrice = 2;//TODO:设置为商品价格
+
+function insertCoin(){
+	switch(event.keyCode)
+	{
+		case 65: currentCoinValue+=1; break; //A键
+		case 83: currentCoinValue+=0.5; break; //S键
+		case 68: currentCoinValue+=0.1; break; //D键
+		default: return;
+	}
+	document.getElementById('coinText').innerHTML=String(currentCoinValue.toFixed(2));
+	if(currentCoinValue>itemPrice)enablePurchaseBtn(purchaseBtnId);
+}
+
+function enablePurchaseBtn(){
+	var btn=document.getElementById('purchaseBtn');
+	btn.className="itemdetail_sumbitpurchasebutton bordered color_ok font30";
+	btn.value="确认购买";
+	btn.disabled=false;
+	//TODO：在此处插入令按钮可按下
+	//btn.onClick=function () {}
+}
+
